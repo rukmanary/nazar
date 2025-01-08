27d9f0c0a172991a5e1edd49c12a894c92cf1a1d
rukmanary <rukmanary@gmail.com>
2025-01-08 16:45:35 +0700

    add async storage

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-
 // Optionally import the services that you want to use
 import { getAuth } from "firebase/auth";
 // import {...} from "firebase/database";
@@ -21,7 +20,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-console.log({ app, apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY });
+// console.log({ app, apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY });
 const auth = getAuth(app);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
